044ee48b95d0ef2eeb5e13c270ab6a295ee55964
Md Mudassir Ali <akhil.mudassir1@gmail.com>
2020-11-20 11:06:07 +0530

    change

--- server.js
+++ server.js
@@ -18,14 +18,22 @@ mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
 mongoose.connection.once('open',function(){ 
     console.log('connection made'); 
     
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-     
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
+    app.use(function (req, res, next) {
+
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+    
+      // Pass to next layer of middleware
       next();
     });
 
